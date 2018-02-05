fba7bc465df1413b449f158d7618d72d32196fdc
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 14:33:26 +0100

    Update Malebolgia.sh

--- Malebolgia.sh
+++ Malebolgia.sh
@@ -8,8 +8,8 @@ gcloud container clusters delete delltechdemo123 --quiet
 echo ""
 echo "$(tput setaf 1) Destroying Local Containers $(tput sgr 0)"
 echo ""
-docker rm -f ConsulDynDNS
+docker rm -f etcd-browserk$instidk
 sleep 1
-docker rm -f receiverK
+docker rm -f honeypot-i
 sleep 1
 docker rm -f etcdk
