8f88cb6751247c7a40ebb9713e8319e13961f79b
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 10:01:42 +0100

    Update GKEProvision.sh

--- GKEProvision.sh
+++ GKEProvision.sh
@@ -12,6 +12,14 @@ kubectl get pods,deployments,services,ingress,configmaps
 
 #After getting the ingress IPs it takes a few minutes for the services to be visible on the external ip
 
+
+publicipKibana=$(kubectl get ing/all-ingress --namespace=default -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+
+publicipelastic=$(kubectl get ing/elasticsearch-ingress --namespace=default -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+
+
+
+
 # kubectl delete -f kubefiles/ -R --namespace=default
 
 # gcloud container clusters delete delltechdemo123
