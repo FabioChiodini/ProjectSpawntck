14f2f16f919fc6dda49d1cc76042b042fd242ed0
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 13:18:27 +0100

    Update GKEProvision.sh

--- GKEProvision.sh
+++ GKEProvision.sh
@@ -7,6 +7,43 @@
 # Log ELK variables in etcd
 # Launch honeypots
 
+#Install jq
+
+echo ""
+echo "$(tput setaf 2) Installing jq $(tput sgr 0)"
+echo ""
+wget http://stedolan.github.io/jq/download/linux64/jq
+
+chmod +x ./jq
+
+sudo cp -p jq /usr/bin
+
+echo ""
+echo "STARTING"
+echo ""
+
+
+#Install local etcd
+
+echo ""
+echo "$(tput setaf 2) Creating a LOCAL etcd instance  $(tput sgr 0)"
+echo ""
+
+ipAWSK=`(curl http://169.254.169.254/latest/meta-data/public-ipv4)`
+#docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcdk quay.io/coreos/etcd -name etcd0 -advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -initial-advertise-peer-urls http://${ipAWSK}:2380 -listen-peer-urls http://0.0.0.0:2380 -initial-cluster-token etcd-cluster-1 -initial-cluster etcd0=http://${ipAWSK}:2380 -initial-cluster-state new
+
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+    --name etcdk quay.io/coreos/etcd:v3.1.0-rc.1 \
+    /usr/local/bin/etcd \
+    --name etcd0 \
+    --advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 \
+    --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+    --initial-advertise-peer-urls http://${ipAWSK}:2380 \
+    --listen-peer-urls http://0.0.0.0:2380 \
+    --initial-cluster-token etcd-cluster-1 \
+    --initial-cluster etcd0=http://${ipAWSK}:2380 -initial-cluster-state new
+
+
 
 
 
