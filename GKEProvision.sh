aff19f38503240a45200a542cf0280332311b9e4
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 11:50:10 +0100

    Update GKEProvision.sh

--- GKEProvision.sh
+++ GKEProvision.sh
@@ -18,8 +18,11 @@ publicipKibana=$(kubectl get ing/all-ingress --namespace=default -o jsonpath="{.
 publicipelastic=$(kubectl get ing/elasticsearch-ingress --namespace=default -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
 
 
+# Kubernetes Setup
 
+kubernetescontext=$(kubectl config view -o jsonpath="{.current-context}")
 
+kubcluster=$(kubectl config view -o jsonpath='{.clusters[?(@.name == "gke_tactile-phalanx-189106_us-central1-a_delltechdemo123")].cluster.server}')
 # kubectl delete -f kubefiles/ -R --namespace=default
 
 # gcloud container clusters delete delltechdemo123
