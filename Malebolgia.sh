667250991a23df06ad08fcd42f089c725e417964
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 15:10:34 +0100

    Update Malebolgia.sh

--- Malebolgia.sh
+++ Malebolgia.sh
@@ -1,4 +1,12 @@
+echo ""
+echo "$(tput setaf 1)Destroying ELK Setup $(tput sgr 0)"
+echo ""
+
 kubectl delete -f kubefiles/ -R --namespace=default
+
+echo ""
+echo "$(tput setaf 1)Destroying Kubernetes Cluster $(tput sgr 0)"
+echo ""
 
 gcloud container clusters delete delltechdemo123 --quiet
 
