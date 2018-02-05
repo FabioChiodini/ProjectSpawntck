4c948ee1be8390bcd7be93cefb2971186c94a81b
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-05 11:56:14 +0100

    Update GKEProvision.sh

--- GKEProvision.sh
+++ GKEProvision.sh
@@ -23,6 +23,7 @@ publicipelastic=$(kubectl get ing/elasticsearch-ingress --namespace=default -o j
 kubernetescontext=$(kubectl config view -o jsonpath="{.current-context}")
 
 kubcluster=$(kubectl config view -o jsonpath='{.clusters[?(@.name == "gke_tactile-phalanx-189106_us-central1-a_delltechdemo123")].cluster.server}')
+
 # kubectl delete -f kubefiles/ -R --namespace=default
 
 # gcloud container clusters delete delltechdemo123
