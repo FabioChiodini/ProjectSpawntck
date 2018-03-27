c4c28448d7b59b098b4bd41890db2242709aac19
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-03-27 11:37:30 +0100

    Update GKEProvision.sh

--- GKEProvision.sh
+++ GKEProvision.sh
@@ -67,6 +67,8 @@ echo ""
 #removing CPU limits
 kubectl delete limitrange limits
 
+echo ""
+
 #Adding cluster-admin permissions
 export GCP_USER=$(gcloud config get-value account | head -n 1)
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$GCP_USER
