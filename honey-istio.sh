75d9d1f46d8a1809b15500860a5cef8ada03ffb0
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-03 16:15:35 +0200

    Update honey-istio.sh

--- honey-istio.sh
+++ honey-istio.sh
@@ -3,4 +3,20 @@
 # Deploys Honeypot
 # Deploys ingress
 
+#Dinamycally read the location of the honeypot receiver
 
+# sample value for your variables
+MYDESTVALUE="nginx:latest"
+
+echo ""
+echo $MYDESTVALUE
+echo ""
+
+# read the yml template from a file and substitute the string 
+# {{MYVARNAME}} with the value of the MYVARVALUE variable
+template=`cat "honeypot-istio/honey-istio-deployment.yaml" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
+
+echo $template
+
+# apply the yml with the substituted value
+#echo "$template" | kubectl apply -f -
