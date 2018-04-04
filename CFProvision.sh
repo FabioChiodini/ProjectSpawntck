88267eefc46a33ee74a666c229b983b05d401d15
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-04 17:25:31 +0200

    Update CFProvision.sh

--- CFProvision.sh
+++ CFProvision.sh
@@ -23,7 +23,7 @@ ipAWSK=`(curl http://127.0.0.1:4001/v2/keys/maininstance/ip | jq '.node.value' |
 #ELK stack
 publicipkibana=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipkibana | jq '.node.value' | sed 's/.//;s/.$//')`
 publicipelastic=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipelastic | jq '.node.value' | sed 's/.//;s/.$//')`
-publiciplogstash=`(curl http://127.0.0.1:4001/v2/keys/elk/publiciplogstash | jq '.node.value' | sed 's/.//;s/.$//')`
+publicipnginxproxy=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #Honeypots
 
@@ -31,6 +31,30 @@ HoneypotImageK=`(curl http://127.0.0.1:4001/v2/keys/honeypots/containername | jq
 HoneypotPortK=`(curl http://127.0.0.1:4001/v2/keys/honeypots/honeypotport | jq '.node.value' | sed 's/.//;s/.$//')`
 publiciplogstash=`(curl http://127.0.0.1:4001/v2/keys/honeypots/receiverip | jq '.node.value' | sed 's/.//;s/.$//')`
 ReceiverPortK=`(curl http://127.0.0.1:4001/v2/keys/honeypots/receiverport | jq '.node.value' | sed 's/.//;s/.$//')`
+
+#Customizing the manifest yml
+
+MYDESTVALUE=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.node.value' | sed 's/.//;s/.$//')`
+
+echo ""
+echo "Address of Honeypot receiver"
+echo $MYDESTVALUE
+echo ""
+
+# read the yml template from a file and substitute the string 
+  # {{MYVARNAME}} with the value of the MYVARVALUE variable
+  template=`cat "cf/template/manifesttemplate.yml" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
+  destdirk=cf/template/manifesttemplate-1.yml
+echo "$template" > "$destdirk"
+
+#adds a unique name
+MYNAMEVALUE=-UNI1-$instidk
+# read the yml template from a file and substitute the string 
+  # {{MYVARNAME}} with the value of the MYVARVALUE variable
+  template=`cat "cf/template/manifesttemplate-1.yml" | sed "s/{{MYUNIQUENAME}}/$MYNAMEVALUE/g"`
+  destdirk=cf/manifest.yml
+echo "$template" > "$destdirk"
+
 
 cf api $cfapik1
 
