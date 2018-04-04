f8bf50c618511d07a484f10e97cf8a6957bac53c
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-04 10:19:44 +0200

    Update honey-istio.sh

--- honey-istio.sh
+++ honey-istio.sh
@@ -23,35 +23,37 @@ MYDESTVALUE=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.n
 istioinstalled=`(curl http://127.0.0.1:4001/v2/keys/istio/installed | jq '.node.value' | sed 's/.//;s/.$//')`
 #http://127.0.0.1:4001/v2/keys/istio/installed
 
-if [ $istioinstalled -eq 0 ]; then
+if [ $istioinstalled -eq 1 ]; then
+  #Dinamycally read the location of the honeypot receiver
+  # sample value for your variables
+  #MYDESTVALUE="nginx:latest"
+
+  echo ""
+  echo "Address of Honeypot receiver"
+  echo $MYDESTVALUE
+  echo ""
+
+  # read the yml template from a file and substitute the string 
+  # {{MYVARNAME}} with the value of the MYVARVALUE variable
+  template=`cat "honeypot-istio/honey-istio-deployment.yaml" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
+
+  #echo $template
+
+  echo ""
+  echo "$(tput setaf 2) Launching an honeypot instance in Kubernetes (with istio service mesh) logging to $MYDESTVALUE $(tput sgr 0)"
+  echo ""
+
+  # apply the yml with the substituted value
+  echo "$template" | kubectl apply -f -
+
+else
   echo ""
   echo "$(tput setaf 1) istio is NOT INSTALLED $(tput sgr 0)"
   echo "$(tput setaf 1) istio is NOT INSTALLED $(tput sgr 0)"
   echo "$(tput setaf 1) istio is NOT INSTALLED $(tput sgr 0)"
+  echo "$(tput setaf 1) EXITING $(tput sgr 0)"
   echo""
 fi
 
 
 
-#Dinamycally read the location of the honeypot receiver
-
-# sample value for your variables
-#MYDESTVALUE="nginx:latest"
-
-echo ""
-echo "Address of Honeypot receiver"
-echo $MYDESTVALUE
-echo ""
-
-# read the yml template from a file and substitute the string 
-# {{MYVARNAME}} with the value of the MYVARVALUE variable
-template=`cat "honeypot-istio/honey-istio-deployment.yaml" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
-
-#echo $template
-
-echo ""
-echo "$(tput setaf 2) Launching an honeypot instance in Kubernetes (with istio service mesh) logging to $MYDESTVALUE $(tput sgr 0)"
-echo ""
-
-# apply the yml with the substituted value
-echo "$template" | kubectl apply -f -
