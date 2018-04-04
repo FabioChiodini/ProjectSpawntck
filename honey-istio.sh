d2f1a63bf1265a6b92d56bfd0aca9a1815dca970
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-04 09:24:29 +0200

    Update honey-istio.sh

--- honey-istio.sh
+++ honey-istio.sh
@@ -3,12 +3,32 @@
 # Deploys Honeypot
 # Deploys ingress
 
+#Must use Cloud1 for accounts (any way to change this?)
+#Some variables are modified later by fetching data from etcd
+. /home/ec2-user/Cloud1
+echo "loaded Config file"
+
+echo ""
+echo "STARTING"
+echo ""
+
+
+echo ""
+echo "$(tput setaf 2) Loading env variables from etcd $(tput sgr 0)"
+echo ""
+#Variables needed
+
+#gets data from previous run
+MYDESTVALUE=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.node.value' | sed 's/.//;s/.$//')`
+
+
 #Dinamycally read the location of the honeypot receiver
 
 # sample value for your variables
-MYDESTVALUE="nginx:latest"
+#MYDESTVALUE="nginx:latest"
 
 echo ""
+echo "Address of Honeypot receiver"
 echo $MYDESTVALUE
 echo ""
 
