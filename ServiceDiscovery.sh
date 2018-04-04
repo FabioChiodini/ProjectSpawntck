13aa308e3babdee436abca5d86ed78d88657db4d
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-04 11:22:34 +0200

    Create ServiceDiscovery.sh

---
+++ ServiceDiscovery.sh
@@ -0,0 +1,114 @@
+#Overview
+# Load config files
+# Prepare etcd and other infrastructure services
+# Connect to Kubernetes cluster
+# Log Kubernetes cluster variables in etcd
+# Execute ELK on Kubernetes
+# Log ELK variables in etcd
+# Launch honeypots
+
+
+#Load config files
+#Load Env variables from File (maybe change to DB)
+#using /home/ec2-user/Cloud1
+#source /home/ec2-user/Cloud1
+. /home/ec2-user/Cloud1
+echo ""
+echo "Loaded Config file"
+echo ""
+echo "$(tput setaf 2) Starting $VM_InstancesK Instances in AWS $(tput sgr 0)"
+if [ $GCEKProvision -eq 1 ]; then
+  echo "$(tput setaf 2) Starting $GCEVM_InstancesK Instances in GCE $(tput sgr 0)"
+fi
+echo "$(tput setaf 2) Starting $Container_InstancesK Container Instances $(tput sgr 0)"
+
+
+#Install jq
+
+echo ""
+echo "$(tput setaf 2) Installing jq $(tput sgr 0)"
+echo ""
+wget http://stedolan.github.io/jq/download/linux64/jq
+
+chmod +x ./jq
+
+sudo cp -p jq /usr/bin
+
+
+#echo "Adding istioctl to path"
+#export PATH=/bin/istio:$PATH
+
+
+echo ""
+echo "STARTING"
+echo ""
+
+
+#Install local etcd
+
+echo ""
+echo "$(tput setaf 2) Creating a LOCAL etcd instance  $(tput sgr 0)"
+echo ""
+
+ipAWSK=`(curl http://169.254.169.254/latest/meta-data/public-ipv4)`
+#docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcdk quay.io/coreos/etcd -name etcd0 -advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -initial-advertise-peer-urls http://${ipAWSK}:2380 -listen-peer-urls http://0.0.0.0:2380 -initial-cluster-token etcd-cluster-1 -initial-cluster etcd0=http://${ipAWSK}:2380 -initial-cluster-state new
+
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+    --name etcdk quay.io/coreos/etcd:v3.1.0-rc.1 \
+    /usr/local/bin/etcd \
+    --name etcd0 \
+    --advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 \
+    --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+    --initial-advertise-peer-urls http://${ipAWSK}:2380 \
+    --listen-peer-urls http://0.0.0.0:2380 \
+    --initial-cluster-token etcd-cluster-1 \
+    --initial-cluster etcd0=http://${ipAWSK}:2380 -initial-cluster-state new
+
+
+#Install local etcd browser 
+
+echo ""
+echo "$(tput setaf 2) Creating a LOCAL etcd Browser instance  $(tput sgr 0)"
+echo ""
+
+  #creates name
+  etcdbrowserkVMName=etcd-browserk$instidk
+  
+  publicipetcdbrowser=$ipAWSK
+  
+  #launches etcd-browser containerized
+  docker run -d --name etcd-browserk -p 0.0.0.0:8000:8000 --env ETCD_HOST=$DynDNSK kiodo/etcd-browser:latest
+  
+  #Register etcd-browser in etcd
+  curl -L http://127.0.0.1:4001/v2/keys/etcd-browser/name -XPUT -d value=$etcdbrowserkVMName
+  curl -L http://127.0.0.1:4001/v2/keys/etcd-browser/ip -XPUT -d value=$publicipetcdbrowser
+  curl -L http://127.0.0.1:4001/v2/keys/etcd-browser/port -XPUT -d value=8000
+  curl -L http://127.0.0.1:4001/v2/keys/etcd-browser/address -XPUT -d value=$publicipetcdbrowser:8000
+
+echo ----
+echo "$(tput setaf 6) $etcdbrowserkVMName RUNNING ON $publicipetcdbrowser:8000 $(tput sgr 0)"
+echo "$(tput setaf 4) publicipetcdbrowser=$publicipetcdbrowser $(tput sgr 0)"
+echo ----
+
+#register local ip and dns name in etcd
+myipK=$( dig +short $DynDNSK @8.8.8.8)
+curl -L http://127.0.0.1:4001/v2/keys/maininstance/ip -XPUT -d value=$myipK
+fqnK=$(nslookup $myipK)
+fqnK=${fqnK##*name = }
+fqnK=${fqnK%.*}
+#echo $fqn
+curl -L http://127.0.0.1:4001/v2/keys/maininstance/name -XPUT -d value=$fqnK
+
+#register instance id in etcd
+curl -L http://127.0.0.1:4001/v2/keys/maininstance/uniqueinstanceid -XPUT -d value=$instidk
+
+#setting istio as not installed
+curl -L http://127.0.0.1:4001/v2/keys/istio/installed -XPUT -d value=0
+
+echo ----
+echo "$(tput setaf 2) Cheeck the service discovery status at $publicipetcdbrowser:8000 $(tput sgr 0)"
+echo "$(tput setaf 2) publicipetcdbrowser=$publicipetcdbrowser $(tput sgr 0)"
+echo ----
+
+echo "Demo by @FabioChiodini"
+
