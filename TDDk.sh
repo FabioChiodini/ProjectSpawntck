f7d289c333f5aac411b3c5043bc43a2fd780f37b
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-09 08:35:44 +0200

    Update TDDk.sh

--- TDDk.sh
+++ TDDk.sh
@@ -2,7 +2,7 @@
 #Loads variables
 #Must use Cloud1 for accounts (any way to change this?)
 #Some variables are modified later by fetching data from etcd
-. /home/ec2-user/Cloud1
+. /home/$USER/Cloud1
 echo "loaded Config file"
 
 
