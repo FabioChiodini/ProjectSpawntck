83cf2a79df84cd5e226b3eb29f68c1972c54132b
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-13 11:26:06 +0200

    Update Riffprovision.sh

--- Riffprovision.sh
+++ Riffprovision.sh
@@ -2,6 +2,9 @@
 
 #Injects the ELK ip into the container code
 
+#gets data from previous run
+MYDESTVALUE=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.node.value' | sed 's/.//;s/.$//')`
+
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
 template=`cat "serverless/honeypot.py" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
