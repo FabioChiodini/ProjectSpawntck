380c37bac0df368204db92d398088fefec75f9b4
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-13 11:41:46 +0200

    Update Riffprovision.sh

--- Riffprovision.sh
+++ Riffprovision.sh
@@ -5,16 +5,29 @@
 #gets data from previous run
 MYDESTVALUE=`(curl http://127.0.0.1:4001/v2/keys/elk/publicipnginxproxy | jq '.node.value' | sed 's/.//;s/.$//')`
 
+publicipriff=`(curl http://127.0.0.1:4001/v2/keys/riff/publicipriff | jq '.node.value' | sed 's/.//;s/.$//')`
+
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
 template=`cat "serverless/honeypot.py" | sed "s/{{MYDESTNAME}}/$MYDESTVALUE/g"`
 destdirk=serverless/container/honeypot.py
 echo "$template" > "$destdirk"
 
+echo ""
+echo "Building a pushing an updated tcfaas container"
+echo ""
+
+
 
 #cd serverless
 #docker build -t kiodo/receiver:latest .
 docker build -t kiodo/tcfaas:latest serverless/container
+
+#docker login
+
+
+#docker push
+#docker push kiodo/tcfaas:latest
 
 
 #create topic
@@ -28,3 +41,8 @@ kubectl get deployment tcfaas
 
 #checks that no pod are started (it's serverless!!)
 kubectl get pods
+
+echo ""
+echo " Access the function using"
+echo " curl http://${HTTP_GATEWAY_IP}/requests/tcfaas"
+echo ""
