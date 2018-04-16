bc99717a02e5c2a3dd9502864d7375c4a94632be
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-04-16 16:03:05 +0200

    Update SpawnSwarmtcK.sh

--- SpawnSwarmtcK.sh
+++ SpawnSwarmtcK.sh
@@ -55,7 +55,8 @@ ipAWSK=`(curl http://169.254.169.254/latest/meta-data/public-ipv4)`
 #docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcdk quay.io/coreos/etcd -name etcd0 -advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -initial-advertise-peer-urls http://${ipAWSK}:2380 -listen-peer-urls http://0.0.0.0:2380 -initial-cluster-token etcd-cluster-1 -initial-cluster etcd0=http://${ipAWSK}:2380 -initial-cluster-state new
 
 docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
-    --name etcdk quay.io/coreos/etcd:v3.1.0-rc.1 \
+    #--name etcdk quay.io/coreos/etcd:v3.1.0-rc.1 \
+    --name etcdk quay.io/coreos/etcd:v3.1.13 \
     /usr/local/bin/etcd \
     --name etcd0 \
     --advertise-client-urls http://${ipAWSK}:2379,http://${ipAWSK}:4001 \
